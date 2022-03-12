d8753194829a32e8fe2ce06435e31eaddeb5d082
Reitumetse <you@example.com>
2022-03-11 18:38:22 -0800

    print combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,14 +14,22 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 <= 9; num2++)
 		{
-			putchar(num1 + '0');
-			putchar(num2 + '0');
 
-			if (!(num1 == 9 && num2 == 9))
+			if (!(num1 == 9 && num2 == 9) && !(num1 == 0 && num2 == 1))
 			{
 				putchar(',');
 				putchar(' ');
 			}
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+
+			/**
+			 * if (!(num1 == 9 && num2 == 9))
+			 *{
+			 *	putchar(',');
+			 *	putchar(' ');
+			  }
+			 */
 		}
 	}
 	putchar('\n');
