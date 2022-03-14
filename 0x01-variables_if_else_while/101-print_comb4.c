8acc5912aa1983d33d43f9ec076bf1b9509e1d2e
Reitumetse <you@example.com>
2022-03-14 06:27:52 -0700

    Print combinations of 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,13 +17,6 @@ int main(void)
 	{
 	for (num3 = num2 + 1; num3 <= 9; num3++)
 	{
-	/**
-	*if (!(num == 9 && num2 == 9 && num3 == 9) && !(num == 0 && num2 == 1 && num3 == 2))
-	*{
-	*	putchar(',');
-	*	putchar(' ');
-	}
-	*/
 		putchar(num + '0');
 		putchar(num2 + '0');
 		putchar(num3 + '0');
