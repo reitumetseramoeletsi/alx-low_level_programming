10685b367dbfe21f23c53a59f83f88b5398301c5
Reitumetse <you@example.com>
2022-03-15 09:52:56 -0700

    Print absolute number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,12 +3,13 @@
 /**
  * _abs - Prints absolute number of a number
  *
+ *@n: Parameter given to abs
  *
  * Return: absolute number
  */
 int _abs(int n)
 {
-	if (n != 0)
+	if (!(n == 0))
 		return (n);
 	else
 		return (0);
