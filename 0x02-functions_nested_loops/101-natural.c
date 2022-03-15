efc3ea47834d9420b0f385703a4d469f426f042c
Reitumetse <you@example.com>
2022-03-15 16:41:21 -0700

    Print natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main Printsnatural numbers
+ *
+ * Returns: Always 0
+ */
+int main(void)
+{
+	int a,b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
+	}
+	printf("%d\n", b);
+
+	return (0);
+}
