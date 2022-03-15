169990bf609555650e984990e68937aae3f40fb6
Reitumetse <you@example.com>
2022-03-15 09:35:18 -0700

    Print absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - Prints absolute number of a number
+ *
+ *
+ * Return: absolute number
+ */
+int _abs(int n)
+{
+	if (n != 0)
+		return (n);
+	else
+		return (0);
+	putchar('\n');
+}
