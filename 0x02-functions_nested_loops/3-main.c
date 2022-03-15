634d40f7861cde81d2c3674f926194cc39922ad6
Reitumetse <you@example.com>
2022-03-15 16:30:02 -0700

    Print numbers from n

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -7,6 +7,13 @@
  */
 int main(void)
 {
-	times_table();
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	_putchar('\n');
 	return (0);
 }
