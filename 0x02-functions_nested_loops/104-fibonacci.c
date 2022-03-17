cd9edee9c7d78d29ded26c0744d82a64bc17528b
Reitumetse <you@example.com>
2022-03-16 19:11:00 -0700

    print fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,23 @@
  */
 int main(void)
 {
-	int n1, n2, next, i;
+	double n1, n2, next;
+	int i;
 
 	n1 = 1;
 	n2 = 2;
 	next = n1 + n2;
 	i = 3;
 
-	printf("%d, %d, ", n1, n2);
+	printf("%.0f, %.0f ", n1, n2);
 	while (i < 98)
 	{
-		printf("%d, ", next);
+		printf("%.0f, ", next);
 		n1 = n2;
 		n2 = next;
 		next = n1 + n2;
 		i++;
 	}
-	printf("%d\n", next);
+	printf("%.0f\n", next);
 	return (0);
 }
