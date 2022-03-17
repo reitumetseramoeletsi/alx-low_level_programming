43d6fd98c8dc3c2f9f3d4a2c168c9d409c18b96f
Reitumetse <you@example.com>
2022-03-16 18:55:36 -0700

    print fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,22 +8,20 @@
 int main(void)
 {
 	int i;
-	double n1, n2, next;
+	unsigned int n1, n2, next;
 
 	n1 = 1;
 	n2 = 2;
 	next = n1 + n2;
 
-	printf("%f, %f, ", n1, n2);
+	printf("%u, %u, ", n1, n2);
 	for (i = 4; i <= 50; i++)
 	{
-		printf("%f, ", next);
+		printf("%u, ", next);
 		n1 = n2;
 		n2 = next;
 		next = n1 + n2;
-		if (i == 46)
-			printf("%f + %f = %f ", n1, n2, n1 + n2);
 	}
-	printf("%f\n", next);
+	printf("%u\n", next);
 	return (0);
 }
