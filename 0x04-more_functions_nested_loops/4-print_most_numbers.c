ea322b2baae7baf4095b82903310701d4bdfab8e
Reitumetse <you@example.com>
2022-03-17 14:33:56 -0700

    Functions and nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0-9
+ * print_most_numbers - prints numbers from 0-9
  */
 void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c<= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 		if (c != '2' && c != '4')
 			_putchar(c);
 	_putchar('\n');
