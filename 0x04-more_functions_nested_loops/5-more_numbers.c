cc1bd280658ae6185f873fdf5a9b021f452ff9f8
Reitumetse <you@example.com>
2022-03-17 14:37:56 -0700

    Functions and nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,10 +8,9 @@ void more_numbers(void)
 	char c;
 	int i;
 
-	for (i = 0; i <=10; i++)
+	for (i = 0; i <= 10; i++)
 	{
-
-		for (c = 0; c<= 14; c++)
+		for (c = 0; c <= 14; c++)
 		{
 			if (c > 9)
 				_putchar((c / 10) + '0');
