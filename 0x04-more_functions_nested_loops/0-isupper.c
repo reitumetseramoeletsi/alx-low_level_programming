00c1776134e05083c01a9e13224b677554c66a61
Reitumetse <you@example.com>
2022-03-17 08:22:30 -0700

    print upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for uppercase letter
+ *
+ * @c: given variable
+ *
+ * Return: Always 0.
+ */
+int _is upper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
