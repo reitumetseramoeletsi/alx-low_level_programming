1f9c9158641b6ee3960451fa2471d1a0f4f5cbe4
Reitumetse <you@example.com>
2022-03-22 16:19:01 -0700

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,6 +6,7 @@
  */
 void print_rev(char *str)
 {
+	/**
 	int i = 0;
 
 	while (*(str + i) != '\0')
@@ -16,5 +17,14 @@ void print_rev(char *str)
 	{
 		_putchar(str[i]);
 		i--;
-	} _putchar('\n');
+	}
+	*/
+	int i;
+
+	for(i = 0; str[i] != '\0'; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i++)
+		_putchar(str[i]);
+       	_putchar('\n');
 }
