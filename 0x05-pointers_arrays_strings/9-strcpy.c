b8108cb9fe02569bb58443422bafddee4e224fc0
Reitumetse <you@example.com>
2022-03-23 03:56:16 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,6 +5,8 @@
 * _strcpy - Copies the string
 * @dest: destination
 * @src: second arg
+*
+* Return: Poiter to dest
 */
 char *_strcpy(char *dest, char *src)
 {
@@ -16,5 +18,6 @@ char *_strcpy(char *dest, char *src)
 	for (; x < i; x++)
 		dest[x] = src[x];
 	dest[x] = '\0';
-	return(dest);
+
+	return (dest);
 }
