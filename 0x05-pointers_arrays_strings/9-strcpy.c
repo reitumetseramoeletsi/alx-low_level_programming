7a50edf5dc753509d85392547794e22529077ff2
Reitumetse <you@example.com>
2022-03-23 03:52:49 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@ char *_strcpy(char *dest, char *src)
 	int i = 0;
 	int x = 0;
 
-	while (*(src +1) != '\0')
+	while ((src[i]) != '\0')
 		i++;
 	for (; x < i; x++)
 		dest[x] = src[x];
-	dest[i] = '\0';
+	dest[x] = '\0';
 	return(dest);
 }
