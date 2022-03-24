941284b272d96de5063aa1c96589341403056a78
Reitumetse <you@example.com>
2022-03-24 13:06:43 -0700

    copy string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,8 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: (< 0) if first string is less than second (> 0) if 2nd st * ring is less than the 1st and 0 if they are equal
+ * Return: (< 0) if first string is less than second (> 0)
+ * if 2nd st * ring is less than the 1st and 0 if they are equal
  */
 int _strcmp(char *s1, char *s2)
 {
