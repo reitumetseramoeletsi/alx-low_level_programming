58d2c5e1d1507e21ecb5ebfb96d62f6a81fc853b
Reitumetse <you@example.com>
2022-03-24 09:11:48 -0700

    concatinate n charecters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates srtings only n bytes of the other
+ * @dest: The destination string
+ * @src: The first string
+ * @n: n bytes to concatenate
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+		i++;
+	
+	j = 0;
+	while (j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+return (dest);
+}
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -6,3 +6,15 @@
  *
  * Return: Always 0 on Success
  */
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
+
+	printf("%s\n", s1);
+	printf("%s\n", s2);
+	ptr = _strncat(s1, s2, 1);
+	printf("%s\n", s1);
+	return (0);
+}
