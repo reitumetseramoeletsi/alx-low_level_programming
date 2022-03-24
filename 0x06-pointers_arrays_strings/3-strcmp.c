bed90b163c9989a7d3dfe466046337ceee0a43a8
Reitumetse <you@example.com>
2022-03-24 13:18:01 -0700

    copy string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,25 +11,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int j;
 
 	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s[2] != '\0')
 	{
+		if (s1[i] != s2[i])
+			return (s1[1] - s2[i]);
+
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-
-	if (i < j)
-		return (-15);
-
-	else if (j < i)
-		return (15);
-
-	else
-		return (0);
+	return (0);
 }
