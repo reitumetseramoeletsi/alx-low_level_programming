ad0fe17dfa01de02ff479608a7035be67ad4306a
Reitumetse <you@example.com>
2022-03-24 17:34:24 -0700

    Encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,11 +11,11 @@ char *leet(char *s)
 	int j = 0;
 	int i = 0;
 
-	char c[21] = {"a4A4e3E3o0O0t7T7l1L1"}; 
+	char c[21] = {"a4A4e3E3o0O0t7T7l1L1"};
 
 	while (s[i] != '\0')
 	{
-		while(c[j] != '\0')
+		while (c[j] != '\0')
 		{
 			if (s[i] == c[j])
 			{
