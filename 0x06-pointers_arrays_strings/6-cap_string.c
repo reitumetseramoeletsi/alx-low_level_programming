b86ae02d9a1edc74e22478120037db4f86735a4c
Reitumetse <you@example.com>
2022-03-24 17:53:53 -0700

    Encode

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,18 +12,31 @@ char *cap_string(char *s)
 	int i;
 	int j = 0;
 
-	char c[14] = {" \t\n,;.!?\"(){}"};
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
 
-	while (s[j] != '\0')
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		for (i = 0; i < 13; i++)
+		switch (s[i])
 		{
+			case ',';
+			case ';';
+			case '.';
+			case '!';
+			case '?';
+			case '"';
+			case '(';
+			case ')';
+			case '{';
+			case '}';
+			case ' ';
+			case '\n';
+			case '\t';
 			if (s[j] == c[i] && s[j + 1] >= 97)
 			{
 				s[j + 1] = s[(j + 1) - 32];
 			}
 		}
-		j++;
 	}
 
 	return (s);
