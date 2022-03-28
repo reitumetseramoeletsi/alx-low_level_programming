c38af471865091f07fb857ba06f8e5ca72c90e4e
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2022-03-28 19:40:22 +0200

    More string manipulations

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,9 +3,9 @@
 /**
  * *_memset - fills the first n bytes with b
  * @n: the given filling bytes
- * @b: The byte to fill
+ * @b: The byte ito fill
  *
- * Return: modigied pointer to string
+ * Return: modified pointer to string
  */
 char *_memset(char *s, char b, unsigned int n)
 {
