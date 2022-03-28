749d498b450b3fb9a1f134e0860c33478542cc66
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2022-03-28 21:55:58 +0200

    locate a charecter[A

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-    char *s = "hello";
+    char *s = "Hello";
     char *f;
 
     f = _strchr(s, 'l');
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
 
-	while (s[i] != '\0')
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
 	{
 		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
-		i++;
+		return (&s[i]);
 	}
 	return (0);
 }
