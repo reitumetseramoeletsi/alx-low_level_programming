cd30376f9edfa589b41045dba3e2f29a7b5ce29e
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2022-03-28 18:26:26 +0200

    More string manipulations

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills the first n bytes with b
+ * @n: the given filling bytes
+ * @b: The byte to fill
+ *
+ * Return: modigied pointer to string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s [i] = b;
+	}
+
+return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+More of arrays and pointers, We are learning about pointer to pointer and 2-D arrays
