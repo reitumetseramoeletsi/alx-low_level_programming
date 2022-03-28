4bb5ed26023ae724c1adb6189ccc792bf7a17853
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2022-03-29 00:26:57 +0200

    size of prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * _strspn - calculates the size of a string prefix
@@ -17,7 +17,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
 			;
-			if (s[i] = accept[j])
+			if (s[i] == accept[j])
 				size++;
 			if (accept[j] == '\0')
 				return (size);
