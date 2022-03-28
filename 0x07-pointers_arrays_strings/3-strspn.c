1d6f43ecb2d9db0f0e7e2ce59f5543b352749e8a
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2022-03-29 00:36:54 +0200

    size of prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,12 +9,14 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+
 	int i;
 	int j;
+	unsigned int size;
 
-	unsigned int size = 0;
+	size = 0;
 
-	for (i = 0; s[i] != '\0'; i++ )
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
 			;
