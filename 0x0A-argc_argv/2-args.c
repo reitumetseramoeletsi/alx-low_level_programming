265933c7f5cbfd1d1897f649757f2b18b12babe8
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2022-04-02 00:35:49 +0200

    prints number of args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,13 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - program that prints ll arguments it receives.
- * @argc: argument that counts argument input
- * @argv: argument that stores the strings in an array of char* (strings)
+ * main - a program that prints all arguments it receives.
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
  * Return: 0
- */
+ **/
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-return (0);
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
 }
