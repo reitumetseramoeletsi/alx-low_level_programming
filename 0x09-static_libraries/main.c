7ff9e1b89f1fac8c3e0723e511cf777ef47cfa28
your name <you@example.com>
2022-04-07 00:29:55 +0200

    staticlib

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,13 +1,7 @@
 #include "main.h"
 
-/*
- * main - checks the code
- *
- * Return: Always 0 on success
- */
-
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	return (0);
+		_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+			return (0);
 }
