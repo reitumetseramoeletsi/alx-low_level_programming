0493e1424895529f8a90b3d9a9e896b35844eea3
your name <you@example.com>
2022-04-07 20:07:12 +0200

    create memory for array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	mem = malloc(nmemb * size)
+	mem = malloc(nmemb * size);
 
 	if (mem == NULL)
 		return (NULL);
