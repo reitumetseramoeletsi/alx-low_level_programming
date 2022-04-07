26180231453653e388e21dc87028ad1be040f330
your name <you@example.com>
2022-04-07 20:03:07 +0200

    create memory for array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - Allocate memory for an array
+ * @nmemb: Number of members
+ * @size: Size of each member
+ *
+ * Return: pointer to the memory allocated
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *mem;
+	unsigned int i = 0;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem = malloc(nmemb * size);
+	
+	if (mem == NULL)
+		return (NULL);
+
+	while (i < nmemb * size)
+	{
+		mem[i] = 0;
+		i++;
+	}
+	return (mem);
+}
