534512599dc239cd3c32a77c88ad95f79bd5e024
Reitumetse <joycreamoeletsi@gmail.com>
2022-04-11 20:34:20 +0200

    print file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The first entry of the program, Pints the name of the running file
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
