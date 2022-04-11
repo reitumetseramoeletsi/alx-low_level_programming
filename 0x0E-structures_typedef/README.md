bd4a925df4f0f0dbc75ab1e46a342ebdd5c7556f
Reitumetse <joycreamoeletsi@gmail.com>
2022-04-11 21:33:36 +0200

    Create

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Data Structures: How to use them, ducument them and type define them
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Aspects of a dog
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
