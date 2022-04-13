88f504f5b9ee46aa4a782313c9a580bde63ec6e9
Reitumetse <joycreamoeletsi@gmail.com>
2022-04-13 19:54:55 +0200

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,5 @@
 #include "function_pointers.h"
-
-/**
- *print_name - prints name
- *@name: string
- *@f: function pointer
- */
-
-void print_name(char *name, void (*f)(char *))
-{
-if (f != NULL)
-	f(name);
-}#include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  *print_name - prints name
  *@name: string
