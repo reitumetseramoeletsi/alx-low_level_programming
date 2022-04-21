96000fcb2fc6eb89a2600d362168292cc2f0bc3e
Reitumetse <joycreamoeletsi@gmail.com>
2022-04-21 17:31:16 +0200

    update print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,19 @@ size_t print_list(const list_t *h)
 {
 	size_t count;
 
-		for (count = 0; h; count++)
+	for (count = 0; h; count++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+			count++;
+			h = h->next;
+		}
+		if (h->str != NULL)
 		{
 			printf("[%d] %s\n", h->len, h->str);
 			h = h->next;
 		}
+	}
 	return (count);
 }
