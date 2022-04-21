355468a28f7d2fbbc4e617604e1857afb24ef4d0
Reitumetse <joycreamoeletsi@gmail.com>
2022-04-21 09:58:12 +0200

    Print number of list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -22,10 +22,10 @@ size_t print_list(const list_t *h)
 	{
 		while (h)
 		{
-			printf("[%d]%s\n",h -> len, h->str);
+			printf("[%d]%s\n", h->len, h->str);
 			count++;
 			h = h->next;
 		}
-	}	
+	}
 	return (count);
 }
