4252fdee3cc01f0dc78ce7ef975b012a04b87a17
Reitumetse <joycreamoeletsi@gmail.com>
2022-04-21 19:10:01 +0200

    assembly code

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,9 @@
  * before_main - prints before excecution of main
  */
 
-void before_main(void) __attribute__ ((constructor));
+void b(void) __attribute__ ((constructor));
 
-void before_main(void)
+void b(void)
 {
 	printf("You're beat! and yet, you must allow,");
 	printf("\nI bore my house upon my back!\n");
