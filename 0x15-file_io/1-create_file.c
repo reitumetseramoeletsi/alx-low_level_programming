f05923966f221a198b3e050efb10d52f85799f78
Reitumetse <joycreamoeletsi@gmail.com>
2022-05-03 20:50:03 +0200

    Write to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	
-	if (filename == NULL)
+	int fd, len;
+	ssize_t f_write;
 
-	fd = open(filename, O-CREAT | O_WRONLY | O_APPEND, 0600);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		f_write = write(fd, text_content, len - 1);
+		if (f_write == -1)
+		{
+			write(1, "fails", 6);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
 }
