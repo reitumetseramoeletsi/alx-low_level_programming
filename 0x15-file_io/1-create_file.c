69f43bf10ab9e2d516e7ac18ccb8deba5d37540e
Reitumetse <joycreamoeletsi@gmail.com>
2022-05-03 21:00:32 +0200

    Write to file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,17 +5,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+
 /**
- * create_file - Creates a file.
- * standard output.
- * @filename: Name of the file to create.
- * @text_content: NULL terminated string to write to the file.
- * Return: 1 on success, -1 on failure.
+ * create_file - creates a new file or append to it if already exists
+ * @filename: name of the file
+ * @text_content: content to be added to file
+ *
+ * Return: 1 on success and -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, lenght;
-	ssize_t res_write;
+	int fd, len;
+	ssize_t f_write;
 
 	if (filename == NULL)
 		return (-1);
@@ -24,16 +26,16 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		lenght = 0;
-		while (*(text_content + lenght) != '\0')
-			lenght++;
-		res_write = write(fd, text_content, lenght);
-		if (res_write == -1)
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		f_write = write(fd, text_content, len);
+		if (f_write == -1)
 		{
 			write(1, "fails", 6);
 			return (-1);
 		}
 	}
+
 	close(fd);
 	return (1);
 }
